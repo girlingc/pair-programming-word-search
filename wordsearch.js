1a9ff2f55da4a39a4b867be1e93f4dda6994c72a
Christian Girling <girling.christian@gmail.com>
2022-01-19 18:37:39 -0800

    Partner: Aya Okizaki. Wrote for vertical and horizontal

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map((ls) => ls.join(""));
+  
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+      if (l.includes(word)) return true;
     }
-}
-
-module.exports = wordSearch
+  
+    let newArray = [];
+    for (i = 0; i < letters.length; i++) {
+      for (j = 0; j < letters[i].length; j++) {
+        if (letters[i][j] === undefined) continue;
+        if (newArray[j] === undefined) newArray[j] = [];
+  
+        newArray[j][i] = letters[i][j];
+      }
+    }
+  
+    const verticalJoin = newArray.map((ls) => ls.join(""));
+    for (l of verticalJoin) {
+      if (l.includes(word)) return true;
+    }
+  
+    return false;
+  };
+  
+  module.exports = wordSearch;
